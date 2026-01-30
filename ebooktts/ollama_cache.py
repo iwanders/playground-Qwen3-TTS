6d85a9fa11a94b7a0080ba058107b15a7c98e501
Ivor Wanders <ivor@iwanders.net>
2026-01-29 19:44:18 -0500

    Cache with values instead of keys, cache readable json.

--- ebooktts/ollama_cache.py
+++ ebooktts/ollama_cache.py
@@ -1,16 +1,13 @@
 import copy
+import hashlib
 import json
 import logging
 from pathlib import Path
 
 import ollama
-from ollama import ChatResponse
+from ollama import ChatResponse, GenerateResponse
 
 logger = logging.getLogger(__name__)
-
-"""
-Something here is super broken, modifying the system prompt still results in cache hits... 
-"""
 
 
 class OllamaCache:
@@ -38,40 +35,48 @@ class OllamaCache:
             json.dump(self._cache, f, indent=1)
 
     def make_key(self, kwargs):
-        return json.dumps(dict(kwargs), indent=1, sort_keys=True)
+        z = hashlib.md5(
+            json.dumps(dict(kwargs), indent=1, sort_keys=True).encode("utf-8")
+        )
+
+        return z.hexdigest()
 
     def retrieve(self, kwargs):
         key = self.make_key(kwargs)
         if key in self._cache:
             # return a copy to avoid the cache getting modified.
-            return copy.deepcopy(self._cache[key])
+            z: ChatResponse = ChatResponse.model_validate(self._cache[key]["response"])
+            return z
 
-    def insert(self, key, response):
-        self._cache[key] = response
+    def insert(self, key, request, response):
+        response_json = None
+        try:
+            if "message" in response:
+                response_json = json.loads(response.message.content)
+            elif "response" in response:
+                response_json = json.loads(response.response)
+            else:
+                response_json = {}
+        except json.JSONDecodeError as e:
+            pass
+        self._cache[key] = {
+            "request": request,
+            "response": response.model_dump(),
+            "response_json": response_json,
+        }
         self.save_cache()
 
-    def chat(self, **kwargs):
+    def cached_worker(self, fun, kwargs):
         cache_hit = self.retrieve(kwargs)
-        if cache_hit is not None:
-            return cache_hit
-        # Need to do work.
-        #
-        response: ChatResponse = ollama.chat(**kwargs)
-        content = response.message.content
-        key = self.make_key(kwargs)
-        self.insert(key, content)
-        return copy.deepcopy(content)
+        if cache_hit is None:
+            response: ChatResponse = fun(**kwargs)
+            key = self.make_key(kwargs)
+            self.insert(key, kwargs, response)
 
-    def generate(self, **kwargs):
-        cache_hit = self.retrieve(kwargs)
-        if cache_hit is not None:
-            return cache_hit
-        # Need to do work.
-        #
-        response: ChatResponse = ollama.generate(**kwargs)
+        return self.retrieve(kwargs)
 
-        print(response)
-        content = response.response
-        key = self.make_key(kwargs)
-        self.insert(key, content)
-        return copy.deepcopy(content)
+    def chat(self, **kwargs) -> ChatResponse:
+        return self.cached_worker(ollama.chat, kwargs)
+
+    def generate(self, **kwargs) -> GenerateResponse:
+        return self.cached_worker(ollama.generate, kwargs)
