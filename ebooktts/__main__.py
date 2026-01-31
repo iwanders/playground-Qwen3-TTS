9724583b6514bd213cf6880aa846b588208359ef
Ivor Wanders <ivor@iwanders.net>
2026-01-30 19:42:25 -0500

    Rename text processor2.

--- ebooktts/__main__.py
+++ ebooktts/__main__.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 
 from . import chunks
 from .text_extractor import Chapter, Extractor
-from .text_processor2 import Section, TextProcessor
+from .text_processor import Section, TextProcessor
 
 """
 https://github.com/QwenLM/Qwen3-TTS/issues/89#issuecomment-3800731293
